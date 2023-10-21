1997d9096365e158b5f3469aae40331f3954a243
sjyda <sjyda90@gmail.com>
2023-10-20 18:23:40 -0700

    Office & Classroom

---
+++ tilesets/classroom.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="classroom" tilewidth="32" tileheight="32" tilecount="6" columns="6">
+ <image source="../../../../../Downloads/classroom.png" width="192" height="32"/>
+</tileset>
